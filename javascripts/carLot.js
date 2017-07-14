270f33a47c7f4a67bfac3e9484c3271449d1ec7a
Bobby Shunk <Bobby@MacBook-Pro-5.local>
2017-07-14 14:00:39 -0500

    work on setting up correct functions within the IIFE to run the XHR right

--- javascripts/carLot.js
+++ javascripts/carLot.js
@@ -1,53 +1,44 @@
-let container = document.getElementsByClassName("container");
-let input = document.getElementById("input");
-let cars = "";
-let card = [];
+// sets variable carLot to equal an IIFE function while expecting globalScopeCarLot to be passed in from main.js
+var carLot = (function (globalScopeCarLot) {
 
-var CarLot = (function (globalScopeCarLot) {
-  console.log("this function is running")
-  // Define a private scope variable to store cars
+  console.log("this is running");
+  // creates variables to hold elements from HTML
+  let container = document.getElementsByClassName("container");
+  let input = document.getElementById("input");
+  let cars = "";
+  let card = [];
   let _car_inventory = [];  
 
+  // defines function carArray
+  function carArray() {
+    console.log("this is also running");
+    // set array to equal the parsed JSON file
+    _car_inventory = JSON.parse(this.responseText);
+  };  
   // Start building the Object that will be attached
   // to the CarLot.Inventory namespace
-  let inventory = Object.create(null, {
-    loadInventory: function (callback) {
-      var load = new XMLHttpRequest();
-      inventoryLoader.addEventListener("load", function () {
-        // Add each car to the private _car_inventory array
-        load.addEventListener("load", function (){})
-        load.addEventListener("error", XHRFail);
-        load.open("GET", "inventory.json")
-        load.send()
-      });
-    }
-  });
+    let inventory = Object.create(null, {
 
+      loadInventory: function (callback) {
+
+        var load = new XMLHttpRequest();
+
+        inventoryLoader.addEventListener("load", function () {
+
+          // Add each car to the private _car_inventory array
+          load.addEventListener("load", carArray);
+          load.open("GET", "inventory.json")
+          load.send()
+        })
+      }
+    })
   globalScopeCarLot.Inventory = inventory;
   return globalScopeCarLot;
 
   // If this is the first module that gets evaluated,
   // CarLot will be `undefined` and a new empty object
   // will augmented.
-})(CarLot || {});
-
-
-// array = JSON.parse(this.responseText)
-// calls outputCards function and passes array into the function
-// outputCards(array)
-
-
-
-
-
-
-
-
-
-
-
-
-
+})(carLot || {});
 
 
 
